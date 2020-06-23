ce9500bca64edac262d3f2f4a7e4b6128cbdfd2e
shreeraamgithubuser <shreeraaam@outlook.com>
2020-06-23 07:38:46 +0200

    refactor codes

--- udacity_project.py
+++ udacity_project.py
@@ -1,5 +1,8 @@
 import time
+import random
 choices = []
+random_enemy = ['Pirate','Phantom','Medusa','Giant Spider']
+enemy = random.choice(random_enemy)
 
 
 def print_pause(message_to_print):
@@ -8,151 +11,122 @@ def print_pause(message_to_print):
 
 def intro():
     time.sleep(1)
-    print_pause("WELCOME TO THE GAME.\n")
+    print_pause("WELCOME TO THE Adventure GAME.\n")
     print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.\n")
-    print_pause("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.\n")
+    print_pause(f"Rumor has it that a {enemy} is somewhere around here, and has been terrifying the nearby village.\n")
     print_pause("To your right is a dark cave.\n")
     print_pause("In your hand you hold your trusty (but not very effective) dagger.\n")
+
+def main_msg():
+    print_pause("Enter 1 to knock on the door of the house.\n")
+    print_pause("Enter 2 to peer into the cave.\n")
 
 def pirate_attack_msg():
     time.sleep(1)
     print_pause("You approach the door of the house.\n")
-    print_pause("You are about to knock when the door opens and out steps a pirate.\n")
-    print_pause("Eep! This is the pirate's house!\n")
-    print_pause("The pirate attacks you!\n")
+    print_pause(f"You are about to knock when the door opens and out steps a dangerous {enemy}.\n")
+    print_pause(f"Eep! This is the {enemy}'s house!\n")
+    print_pause(f"The {enemy} attacks you!\n")
 
+def cave_msg():
+    time.sleep(1)
+    print_pause("You peer cautiously into the cave.\n")
+    print_pause("It turns out to be only a very small cave.\n")
+    print_pause("Your eye catches a glint of metal behind a rock.\n")
+    print_pause("You have found the magical Sword of Ogoroth.\n")
 
-def main_theme():
+def sword_msg():
+    print_pause("Do you want to pick the Sword of Ogoroth\n")
 
-    while True:
+def sword_pick_msg():
+    print_pause("Great Choice...\n")
+    print_pause("A ranged attack that silences enemies with bolt of electricity is in your possesion.\n")
 
-        print_pause("Enter 1 to knock on the door of the house.\n")
-        print_pause("Enter 2 to peer into the cave.\n")
+def sword_not_picked_msg():
+    print_pause("Be sure you didnt pick the Sword. Anyways All The Best...\n")
 
-        player_response = input("What would you like to do now? (Please enter 1 or 2.)\n")
-        if player_response == '1':
-            fight()
-            break
-        elif player_response == '2':
-            cave()
-            break
-        else:
-            print_pause("Sorry, I don't understand.Please Enter number 1 or 2.\n")
+def cave_repeat_msg():
+    time.sleep(1)
+    print_pause("Oh boy!!!!!!!. We have visited the Cave already and taken the sword of our Choice\n")
+    print_pause("It is useless to come back to the cave and searching the empty place.\n")
 
-def fight():
+def attack_without_sword_msg():
+    time.sleep(1)
+    print_pause("You do your best...\n")
+    print_pause(f"but your dagger is no match for the {enemy}.\n")
+    print_pause("You have been defeated!.\n")
 
-    if "sword_taken" not in choices:
-        pirate_attack_msg()
-        print_pause("You feel a bit under-prepared for this, what with only having a tiny dagger.\n")
-    else:
-        pirate_attack_msg()
-        print_pause("You feel ready for this, what with having your new sword.\n")
-
-    while True:
-
-        fight_response = input("Would you like to (1) fight or (2) run away?\n")
-
-        if fight_response == '1':
-            
-            if "sword_taken" in choices:
-                attack_with_sword()
-                break
-            elif "sword_taken" not in choices:
-                attack_without_sword()
-                break
-        elif fight_response == '2':
-            print_pause("You run back into the field. Luckily, you don't seem to have been followed.\n")
-            main_theme()
-        else:
-            print_pause("Sorry, I don't understand.Please Enter number 1 or 2.\n")
-
-def cave():
-
-    if "sword_taken" in choices:
-        print_pause("Oh boy!!!!!!!. We have visited the Cave already and taken the sword of our Choice\n")
-        print_pause("We cannot enter the cave again\n")
-        main_theme()
-
-    elif "sword_taken" not in choices:
-        print_pause("You peer cautiously into the cave.\n")
-        print_pause("It turns out to be only a very small cave.\n")
-        print_pause("Your eye catches a glint of metal behind a rock.\n")
-        print_pause("You have found the magical Sword of Ogoroth and Sword of Zeus\n")
-        while True:
-            
-            sword_response = input("Would you like to take (1) Sword of Ogoroth  or (2) Sword of Zeus?\n")
-            if sword_response == '1':
-                print_pause("Great Choice...\n")
-                print_pause("A ranged attack that silences enemies with bolt of FireBalls.\n")
-                choices.append("sword_taken")
-                break
-            elif sword_response == '2':
-                print_pause("Great Choice...\n")
-                print_pause("A ranged attack that silences enemies with bolt of electricity.\n")
-                choices.append("sword_taken")
-                break
-            else:
-                print_pause("Sorry, I don't understand.Please Enter number 1 or 2.\n")
-        print_pause("You discard your silly old dagger and take the sword of your choice with you.\n")
-        print_pause("You walk back out to the field.\n")
-        main_theme()
-           
 def attack_with_sword():
-        print_pause("As the gorgon moves to attack, you unsheath your new sword.\n")
-        print_pause("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.\n")
-        print_pause("But the gorgon takes one look at your shiny new toy and runs away!\n")
-        print_pause("You have rid the town of the gorgon. You are victorious!\n")
-        print_pause("YOU WON\n")
-        play_again()
-        
+    time.sleep(1)
+    print_pause(f"As the {enemy} moves to attack, you unleash your new sword.\n")
+    print_pause("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.\n")
+    print_pause(f"But the {enemy} takes one look at your shiny new toy and runs away!\n")
+    print_pause(f"You have rid the {enemy}. You are victorious!\n")
+    print_pause("YOU WON\n")
 
-def attack_without_sword():
 
-        print_pause("You do your best...\n")
-        print_pause("but your dagger is no match for the pirate.\n")
-        print_pause("You have been defeated!.\n")
-        play_again()
-
-def play_again():
-
-    choices.clear()
+def field():
 
     while True:
-
-        play_again_response = input("Would you like to play again? (y/n)\n").lower()
-
-        if play_again_response == 'y':
-            print_pause("Excellent! Restarting the game ...\n")
-            play_game()
-        elif play_again_response == 'n':
+        main_msg()
+        player_response = input("What would you like to do now? (Please enter 1 or 2.)\n")
+        if player_response == "1":
+            pirate_attack_msg()
+            if "player_has_sword" in choices:
+                print_pause("You feel ready for this, what with having your new sword.\n")
+            else:
+                print_pause("You feel a bit under-prepared for this, what with only having a tiny dagger.\n")
+            fight_run()
             break
-    #print_pause("Thank you for supporting the Game.See you soon again...\n")
+        elif player_response == "2":
+            if "player_has_sword" in choices:
+                cave_repeat_msg()
+            else:
+                cave_msg()
+                sword_pick()
         else:
-            print_pause("Sorry, I don't understand.Please Enter number 'y' or 'n'.\n")
+            print_pause("Sorry, I don't understand.Please Enter number 1 or 2.\n")
+
+def fight_run():
     
+    while True:
+        fight_run_response = input("Would you like to (1) fight or (2) run away?\n")
+
+        if fight_run_response == "1":
+            if "player_has_sword" in choices:
+                attack_with_sword()
+            else:
+                attack_without_sword_msg()
+            break
+        elif fight_run_response == "2":
+            print_pause("You run back into the field. Luckily, you don't seem to have been followed.\n")
+            field()
+            break
+        else:
+            print_pause("Sorry, I don't understand.Please Enter number 1 or 2.\n")
+    #field()
+
+def sword_pick():
+
+    while True:
+        sword_msg()
+
+        sword_plr_response = input("If yes 'Press 1' or 'Press 2' for no.\n")
+
+        if sword_plr_response == "1":
+            print_pause("Great Choice...\n")
+            print_pause("A ranged attack that silences enemies with bolt of electricity is in your possesion.\n")
+            choices.append("player_has_sword")
+            break
+        elif sword_plr_response == "2":
+            sword_not_picked_msg()
+            choices.append('')
+            break
+        else:
+            print_pause("Sorry, I don't understand.Please Enter number 1 or 2.\n")
 
 def play_game():
-    intro()
-    main_theme()
+    intro()          
+    field()
 
 play_game()
-
-
-
-
-#def play_again():
-
-    #while True:
-
-        #play_again_response = input("Would you like to play again? (y/n)\n").lower()
-
-        #if play_again_response == 'y':
-            #print_pause("Excellent! Restarting the game ...\n")
-            #choices.clear()
-            #play_game()
-        #elif play_again_response == 'n':
-            #choices.clear()
-            #break
-    #print_pause("Thank you for supporting the Game.See you soon again...\n")
-        #else:
-            #print_pause("Sorry, I don't understand.Please Enter number 'y' or 'n'.\n")
